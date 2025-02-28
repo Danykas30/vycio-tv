e3d84f912637150f9921feb86103449c9f99b740
Danykas30 <parko20a@gmail.com>
2025-02-28 21:04:32 +0200

    Delete phpMyAdmin.sql directory

--- phpMyAdmin.sql/komentarai.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE vytio_tv;
-USE vytio_tv;
-
-CREATE TABLE comments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    comment TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
